27b70753059a25bafc46a08c050ab3107dc33adb
Jack Tanner <jack@jackandtheblockstalk.com>
2022-08-11 21:31:03 +0200

    Use password key for owner and active

--- contracts/id.tonomy/src/id.tonomy.cpp
+++ contracts/id.tonomy/src/id.tonomy.cpp
@@ -1,9 +1,9 @@
 #include <id.tonomy/id.tonomy.hpp>
 #include <eosio.bios/eosio.bios.hpp>
+#include <vector>
 
 namespace idtonomy
 {
-
    void id::newperson(
        name creator,
        checksum256 username_hash,
@@ -14,17 +14,23 @@ namespace idtonomy
    {
       eosio::require_auth(creator);
 
+      // TODO generate new random account name
+      const name randomname = "newrandomname"_n;
+
+      eosiobios::authority owner{}; // zero initialized
+      owner.threshold = 1;
+      eosiobios::key_weight password_key;
+      password_key.weight = 1;
+      password_key.key = password;
+      owner.keys.push_back(password_key);
+
+      eosiobios::bios::newaccount_action newaccountaction("eosio"_n, {get_self(), "active"_n});
+      newaccountaction.send(creator, randomname, owner, owner);
+
       // TODO:
-      // generate new random account name
-      // use password key for owner and active
       // update key with pin
       // update key with fingerprint
       // Store the salt and hashed username in table, with type = Person
-
-      const name randomname = "newrandomname"_n;
-      // const bios::authority owner =
-      eosiobios::bios::newaccount_action newaccountaction("eosio"_n, {get_self(), "active"_n});
-      // modaction.send(creator, randomname, owner, active);
 
       check(false, "Check false");
    }
