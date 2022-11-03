df82472bff72575a54fa09dbecd4cb2657387205
Jack Tanner <jack@jackandtheblockstalk.com>
2022-11-03 14:12:52 +0100

    Delete built contracts script

---
+++ delete-buildt-contracts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -u ## exit if you try to use an uninitialised variable
+set -e ## exit if any statement fails
+
+# Make sure working dir is same as this dir, so that script can be excuted from another working directory
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+echo "Deleting built smart contracts"
+
+CONTRACTS=(
+    "eosio.boot"
+    "eosio.bios"
+    "eosio.bios.tonomy"
+    "eosio.token"
+    "id.tonomy"
+)
+for CONTRACT in "${CONTRACTS[@]}"
+do
+    echo "Deleting smart contract ${CONTRACT} .wasm and .abi files"
+    rm -f "${PARENT_PATH}/contracts/${CONTRACT}/${CONTRACT}.wasm"
+    rm -f "${PARENT_PATH}/contracts/${CONTRACT}/${CONTRACT}.abi"
+done
