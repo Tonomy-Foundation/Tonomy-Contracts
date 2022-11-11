8d4ac9d88e7263d19f74775ba97319bcc6397b08
Jack Tanner <jack@jackandtheblockstalk.com>
2022-11-11 13:15:48 +0100

    Manual install of boost, just in case

--- setup-cpp.sh
+++ setup-cpp.sh
@@ -6,10 +6,16 @@ set -e ## exit if any statement fails
 # Make sure working dir is same as this dir, so that script can be excuted from another working directory
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-echo "Installing c++"
+echo "Installing c++ and libraries"
 sudo apt install c++ -y
 
-echo "Cloning eosio.cdt"
+echo "Setup Boost library"
+wget -O boost_1_67_0.tar.gz https://sourceforge.net/projects/boost/files/boost/1.67.0/boost_1_67_0.tar.gz/download
+mkdir boost
+tar xzvf boost_1_67_0.tar.gz -C boost
+rm boost_1_67_0.tar.gz
+
+echo "Cloning eosio.cdt for libraries"
 git clone git@github.com:EOSIO/eosio.cdt.git
 cd eosio.cdt
 git checkout v1.8.1
