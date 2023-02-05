177eab1890b69404d78c3c6f7cac9dc02663805f
Jack Tanner <jack@jackandtheblockstalk.com>
2023-02-05 23:19:08 +0100

    Gracefully shutdown nodeos

--- blockchain/nodeos.sh
+++ blockchain/nodeos.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
-# command line only args from the chain plugin:
-# disable-replay-opts
-nodeos\
-    -e\
-    -p eosio\
-    --data-dir /data/data-dir\
-    --config-dir /var/config\
-    --disable-replay-opts
+ARG1=$1
 
-# nohup sh -c nodeos -e -p eosio &
-# nodeos -e -p eosio
-# nohup echo "what do you want" > ./log.log &
-# sleep 1
-# cat ./log.log
-# echo "Running nodeos.sh"
+function start {
+    # command line only args from the chain plugin:
+    # disable-replay-opts
+    exec nodeos \
+        -e \
+        -p eosio \
+        --data-dir /data/data-dir \
+        --config-dir /var/config \
+        --disable-replay-opts
+}
 
-# echo "what do you want now?" > /var/config/log.log &
+# Ensures gracefull shutdown of nodeos process. See:
+# https://github.com/EOSIO/eos/issues/4742
+# https://github.com/EOSIO/eos/issues/4462#issuecomment-412772944
+function stop {
+    nodeos_pid=$(pgrep -x nodeos)
+    echo "Nodeos pid: ${nodeosd_pid}"
+    
+    if [ -n "$(ps -p ${nodeosd_pid} -o pid=)" ]; then
+        echo "Send SIGINT"
+        kill -SIGINT ${nodeosd_pid}
+    fi
+
+    while [ -n "$(ps -p ${nodeosd_pid} -o pid=)" ]
+    do
+        sleep 1
+    done
+
+    echo "Process nodeos has finished gracefully"}
+}
+
+if [ "${ARG1}" == "stop" ]
+then
+    stop2
+else
+    start
+fi
--- blockchain/test-docker.sh
+++ blockchain/test-docker.sh
@@ -6,8 +6,10 @@ docker exec -it eosiodocker nodeos-stop.sh
 docker stop eosiodocker
 docker rm eosiodocker
 
+# Make sure you give the context as the root of the repo
 docker build ./.. -f ./Dockerfile --force-rm -t eosiobuild
 
 # sudo rm ${PARENT_PATH}/tmp -rf
-docker run -v ${PARENT_PATH}/tmp:/data --name eosiodocker -d eosiobuild
+# docker run -v ${PARENT_PATH}/tmp:/data --name eosiodocker -d eosiobuild
+docker run --name eosiodocker eosiobuild
 # docker exec -it eosiodocker cleos --help
