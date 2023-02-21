43ffe26d43374dc956b4f195c3e305b7c18933d5
Jack Tanner <jack@jackandtheblockstalk.com>
2023-02-21 15:16:47 +0100

    use better build.sh script

--- contracts/eosio.bios/build.sh
+++ contracts/eosio.bios/build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+ARG1=$1
 
-DOCKER_WORKING_DIR="/contract"
+if [ "$ARG1" == "local" ]; then
+    WORKING_DIR="./contract"
+else
+    WORKING_DIR="/contract"
+fi
 CONTRACT_NAME="eosio.bios"
-docker run -v "${PARENT_PATH}:${DOCKER_WORKING_DIR}"\
-    eosio/eosio.cdt:v1.8.1\
-    eosio-cpp\
-    -abigen\
-    -I "${DOCKER_WORKING_DIR}/include"\
-    -R "${DOCKER_WORKING_DIR}"/ricardian\
-    -contract "${CONTRACT_NAME}"\
-    -o "${DOCKER_WORKING_DIR}/${CONTRACT_NAME}.wasm"\
-    "${DOCKER_WORKING_DIR}/src/${CONTRACT_NAME}.cpp"
+
+BUILD_COMMAND="eosio-cpp -abigen -I ${WORKING_DIR}/include -R ${WORKING_DIR}/ricardian -contract ${CONTRACT_NAME} -o ${WORKING_DIR}/${CONTRACT_NAME}.wasm ${WORKING_DIR}/src/${CONTRACT_NAME}.cpp"
+echo $BUILD_COMMAND
+
+if [ "$ARG1" == "local" ]; then
+    bash -c "${BUILD_COMMAND}"
+else
+    docker run -v "${PARENT_PATH}:${WORKING_DIR}" eosio/eosio.cdt:v1.8.1 bash -c "${BUILD_COMMAND}"
+fi
