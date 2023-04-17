9c454b88a5d91d929a6ee1fc4b427f0918f7f200
Jack Tanner <jack@jackandtheblockstalk.com>
2023-04-17 16:45:31 +0200

    fix: fixed issue with contract build

--- contracts/id.tonomy/build.sh
+++ contracts/id.tonomy/build.sh
@@ -5,13 +5,6 @@ BUILD_METHOD=$1
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "${PARENT_PATH}"
 
-# DOCKER_WORKING_DIR="/contract/id.tonomy"
-# CONTRACT_NAME="id.tonomy"
-# docker run\
-#     -v "${PARENT_PATH}/../eosio.bios/include/eosio.bios:${DOCKER_WORKING_DIR}/include/eosio.bios"\
-#     -v "${PARENT_PATH}/../:${DOCKER_WORKING_DIR}/../"\
-#     -v "${PARENT_PATH}:${DOCKER_WORKING_DIR}"\
-
 function compile_contract_id_tonomy {
     PARENT_PATH=$1
     CONTRACT_NAME=$2
@@ -23,7 +16,7 @@ function compile_contract_id_tonomy {
         WORKING_DIR="/contracts/id.tonomy"
     fi
 
-    BUILD_COMMAND="eosio-cpp -abigen -I ${WORKING_DIR}/include -R ${WORKING_DIR}/ricardian -contract ${CONTRACT_NAME} -o ${WORKING_DIR}/${CONTRACT_NAME}.wasm ${WORKING_DIR}/src/${CONTRACT_NAME}.cpp"
+    BUILD_COMMAND="cdt-cpp -abigen -I ${WORKING_DIR}/include -R ${WORKING_DIR}/ricardian -contract ${CONTRACT_NAME} -o ${WORKING_DIR}/${CONTRACT_NAME}.wasm ${WORKING_DIR}/src/${CONTRACT_NAME}.cpp"
     echo $BUILD_COMMAND
     echo ""
 
