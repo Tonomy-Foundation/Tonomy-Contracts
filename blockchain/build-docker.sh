1c8b400617b43d05e21cd1f79edd2b88995e7bf3
Jack Tanner <jack@jackandtheblockstalk.com>
2023-09-21 10:19:04 +0200

    feat: Enable prometheus plugin

--- blockchain/build-docker.sh
+++ blockchain/build-docker.sh
@@ -7,12 +7,16 @@ set -e ## exit if any statement fails
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "${PARENT_PATH}/.."
 
+# Build image for antelope blockchain
 docker rm -f antelope_blockchain || true
 docker image build --target antelope_blockchain . -f ./blockchain/Dockerfile --force-rm -t antelope_blockchain
 
 # Build Tonomy Contracts
 ./build-contracts.sh
 
-# Create docker image
+# Build image for antelope blockchain with contracts and initalized chain
 docker rm -f tonomy_blockchain_initialized || true
 docker image build --target tonomy_blockchain_initialized . -f ./blockchain/Dockerfile --force-rm -t tonomy_blockchain_initialized
+
+# Run command
+# docker run --name tonomy_blockchain_initialized -p 8888:8888 -p 9101:9101 tonomy_blockchain_initialized
