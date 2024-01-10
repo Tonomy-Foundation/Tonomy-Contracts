a4b40d3465d2ea60385d799bb94c7db28baef237
Jack Tanner <jack@jackandtheblockstalk.com>
2024-01-10 17:05:13 +0100

    feat: Moved id.tmy to eosio.bios.tonomy

--- contracts/eosio.bios.tonomy/build.sh
+++ contracts/eosio.bios.tonomy/build.sh
@@ -5,31 +5,6 @@ BUILD_METHOD=$1
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "${PARENT_PATH}"
 
-function compile_contract_id_tonomy {
-    PARENT_PATH=$1
-    CONTRACT_NAME=$2
-    BUILD_METHOD=$3
+source ../compile_contract.sh
 
-    if [ "$BUILD_METHOD" == "local" ]; then
-        WORKING_DIR="."
-    else
-        WORKING_DIR="/contracts/eosio.bios.tonomy"
-    fi
-
-    BUILD_COMMAND="cdt-cpp -abigen -I ${WORKING_DIR}/include -R ${WORKING_DIR}/ricardian -contract ${CONTRACT_NAME} -o ${WORKING_DIR}/${CONTRACT_NAME}.wasm ${WORKING_DIR}/src/${CONTRACT_NAME}.cpp"
-    echo $BUILD_COMMAND
-    echo ""
-
-    cp -r "${PARENT_PATH}/../id.tmy/include/id.tmy" "${PARENT_PATH}/include/id.tmy"
-    if [ "$BUILD_METHOD" == "local" ]; then
-        bash -c "${BUILD_COMMAND}"
-    else
-        docker run\
-            -v "${PARENT_PATH}:${WORKING_DIR}"\
-            -v "${PARENT_PATH}/..:${WORKING_DIR}/.."\
-            tonomy/antelope bash -c "${BUILD_COMMAND}"
-    fi
-    rm -rf "${PARENT_PATH}/include/id.tmy"
-}
-
-compile_contract_id_tonomy "${PARENT_PATH}" "eosio.bios.tonomy" "${BUILD_METHOD}"
+compile_contract "${PARENT_PATH}" "eosio.bios.tonomy" "${BUILD_METHOD}"
--- contracts/eosio.bios.tonomy/include/eosio.bios.tonomy/eosio.bios.tonomy.hpp
+++ contracts/eosio.bios.tonomy/include/eosio.bios.tonomy/eosio.bios.tonomy.hpp
@@ -12,14 +12,16 @@
 namespace eosiobiostonomy
 {
    using eosio::action_wrapper;
+   using eosio::asset;
    using eosio::check;
    using eosio::checksum256;
    using eosio::ignore;
    using eosio::name;
    using eosio::permission_level;
+   using eosio::print;
    using eosio::public_key;
-   using eosio::asset;
    using eosio::singleton;
+   using std::string;
 
    struct permission_level_weight
    {
@@ -74,6 +76,45 @@ namespace eosiobiostonomy
       EOSLIB_SERIALIZE(block_header, (timestamp)(producer)(confirmed)(previous)(transaction_mroot)(action_mroot)(schedule_version)(new_producers))
    };
 
+   // Create an enum type and an eosio type for enums
+   // https://eosio.stackexchange.com/questions/4950/store-enum-value-in-table
+   enum enum_account_type
+   {
+      Person,
+      Organization,
+      App,
+      Gov,
+      Service
+   };
+   typedef uint8_t account_type;
+
+   enum enum_account_status
+   {
+      Creating_Status,
+      Active_Status,
+      Deactivated_Status,
+      Upgrading_Status
+   };
+   typedef uint8_t account_status;
+
+   std::map<enum_account_type, char> account_type_letters = {
+       {enum_account_type::Person, 'p'},
+       {enum_account_type::App, 'a'},
+       {enum_account_type::Organization, 'o'},
+       {enum_account_type::Gov, 'g'},
+       {enum_account_type::Service, 's'}};
+
+   enum enum_permission_level_name
+   {
+      Owner,
+      Active,
+      Password,
+      Pin,
+      Biometric,
+      Local
+   };
+   typedef uint8_t permission_level_name;
+
    /**
     * The `eosio.bios.tonomy` is the first sample of system contract provided by `block.one` through the EOSIO platform. It is a minimalist system contract because it only supplies the actions that are absolutely critical to bootstrap a chain and nothing more. This allows for a chain agnostic approach to bootstrapping a chain.
     *
@@ -81,10 +122,19 @@ namespace eosiobiostonomy
     */
    class [[eosio::contract("eosio.bios.tonomy")]] bios : public eosio::contract
    {
+   private:
+      uint64_t initial_cpu_weight_allocation = 1000;
+      uint64_t initial_net_weight_allocation = 1000;
+
    public:
       using contract::contract;
+
+      /**
+       * Constructor for the contract, which initializes the _accounts table
+       */
+      bios(name receiver, name code, eosio::datastream<const char *> ds);
+
       static constexpr eosio::symbol system_resource_currency = eosio::symbol("ONO", 4);
-      static constexpr eosio::name idtmy_name = "id.tmy"_n;
       static constexpr eosio::name gov_name = "gov.tmy"_n;
       /**
        * New account action, called after a new account is created. This code enforces resource-limits rules
@@ -161,7 +211,8 @@ namespace eosiobiostonomy
        * @param canceling_auth - the permission that authorizes this action,
        * @param trx_id - the deferred transaction id to be cancelled.
        */
-      [[eosio::action]] void canceldelay(ignore<permission_level> canceling_auth, ignore<checksum256> trx_id) {
+      [[eosio::action]] void canceldelay(ignore<permission_level> canceling_auth, ignore<checksum256> trx_id)
+      {
          require_auth(gov_name);
       }
 
@@ -173,9 +224,9 @@ namespace eosiobiostonomy
        * @param vmversion - reserved, set it to zero.
        * @param code - the code content to be set, in the form of a blob binary..
        */
-      [[eosio::action]] void setcode(name account, uint8_t vmtype, uint8_t vmversion, const std::vector<char> &code) {
-               require_auth(gov_name);
-
+      [[eosio::action]] void setcode(name account, uint8_t vmtype, uint8_t vmversion, const std::vector<char> &code)
+      {
+         require_auth(gov_name);
       }
 
       /**
@@ -247,49 +298,47 @@ namespace eosiobiostonomy
        */
       [[eosio::action]] void activate(const eosio::checksum256 &feature_digest);
 
-
-     /**
-      * The setresparams action sets the price for RAM, the total RAM available, and the RAM fee.
-      * It checks for an entry in the resource_config_table singleton, with `get_self()` as key.
-      * If the entry does not exist, it creates a new entry with the provided RAM price, total RAM available, RAM fee, and sets other parameters to 0.
-      * If the entry exists, it updates the RAM price, total RAM available, and RAM fee values for the existing `get_self()` key.
-      *
-      * @param ram_price - the new price of RAM
-      * @param total_ram_available - the new total RAM available
-      * @param ram_fee - the new RAM fee
-      */
+      /**
+       * The setresparams action sets the price for RAM, the total RAM available, and the RAM fee.
+       * It checks for an entry in the resource_config_table singleton, with `get_self()` as key.
+       * If the entry does not exist, it creates a new entry with the provided RAM price, total RAM available, RAM fee, and sets other parameters to 0.
+       * If the entry exists, it updates the RAM price, total RAM available, and RAM fee values for the existing `get_self()` key.
+       *
+       * @param ram_price - the new price of RAM
+       * @param total_ram_available - the new total RAM available
+       * @param ram_fee - the new RAM fee
+       */
       [[eosio::action]] void setresparams(double ram_price, uint64_t total_ram_available, double ram_fee);
-      
-      /**
-      * Buy RAM action allows an app to purchase RAM. 
-      * It checks the account type of the app, ensures the RAM is being purchased with the correct token, 
-      * and that the amount of tokens being used for the purchase is positive. 
-      * It then calculates the amount of RAM to purchase based on the current RAM price, 
-      * checks if there is enough available RAM, and allocates the purchased RAM to the app.
-      * Finally, it updates the total RAM used and available in the system, and 
-      * transfers the tokens used for the purchase.
-      *
-      * @param dao_owner - the name of the DAO owner account
-      * @param app - the name of the app account purchasing the RAM
-      * @param quant - the amount and symbol of the tokens used for the purchase
-      */
-      [[eosio::action]] void buyram(const name& dao_owner, const name& app, const asset& quant);
 
       /**
-      * Sell RAM action allows an app to sell RAM. 
-      * It checks the account type of the app, ensures the RAM is being sold for the correct token, 
-      * and that the amount of RAM being sold is positive. 
-      * It then calculates the amount of tokens to return based on the current RAM price, 
-      * checks if there is enough RAM being used by the app, and deallocates the sold RAM from the app.
-      * Finally, it updates the total RAM used in the system, and 
-      * transfers the tokens from the sale.
-      *
-      * @param dao_owner - the name of the DAO owner account
-      * @param app - the name of the app account selling the RAM
-      * @param quant - the amount and symbol of the tokens used to sell
-      */
+       * Buy RAM action allows an app to purchase RAM.
+       * It checks the account type of the app, ensures the RAM is being purchased with the correct token,
+       * and that the amount of tokens being used for the purchase is positive.
+       * It then calculates the amount of RAM to purchase based on the current RAM price,
+       * checks if there is enough available RAM, and allocates the purchased RAM to the app.
+       * Finally, it updates the total RAM used and available in the system, and
+       * transfers the tokens used for the purchase.
+       *
+       * @param dao_owner - the name of the DAO owner account
+       * @param app - the name of the app account purchasing the RAM
+       * @param quant - the amount and symbol of the tokens used for the purchase
+       */
+      [[eosio::action]] void buyram(const name &dao_owner, const name &app, const asset &quant);
+
+      /**
+       * Sell RAM action allows an app to sell RAM.
+       * It checks the account type of the app, ensures the RAM is being sold for the correct token,
+       * and that the amount of RAM being sold is positive.
+       * It then calculates the amount of tokens to return based on the current RAM price,
+       * checks if there is enough RAM being used by the app, and deallocates the sold RAM from the app.
+       * Finally, it updates the total RAM used in the system, and
+       * transfers the tokens from the sale.
+       *
+       * @param dao_owner - the name of the DAO owner account
+       * @param app - the name of the app account selling the RAM
+       * @param quant - the amount and symbol of the tokens used to sell
+       */
       [[eosio::action]] void sellram(eosio::name dao_owner, eosio::name app, eosio::asset quant);
-
 
       /**
        * Require activated action, asserts that a protocol feature has been activated
@@ -297,6 +346,173 @@ namespace eosiobiostonomy
        * @param feature_digest - hash of the protocol feature to check for activation.
        */
       [[eosio::action]] void reqactivated(const eosio::checksum256 &feature_digest);
+
+      /**
+       * Create a new account for a person
+       *
+       * @details Creates a new account for a person.
+       *
+       * @param username_hash - hash of the username of the account
+       * @param password_key - public key generated from the account's password
+       * @param password_salt - salt used to generate the password_key with the password
+       */
+      [[eosio::action]] void newperson(
+          checksum256 username_hash,
+          public_key password_key,
+          checksum256 password_salt);
+
+      /**
+       * Sets the account type for a given account
+       *
+       * @param account_name - name of the account
+       * @param acc_type - account type to be set
+       */
+      [[eosio::action]] void setacctype(name account_name, account_type acc_type);
+
+      /**
+       * Create a new account for an app and registers it's details
+       *
+       * @details Creates a new account for an app and registers it's details.
+       *
+       * @param name - name of the app
+       * @param description - description of the app
+       * @param username_hash - hash of the username
+       * @param logo_url - url to the logo of the app
+       * @param origin - domain associated with the app
+       * @param password_key - public key generated from the account's password
+       */
+      [[eosio::action]] void newapp(
+          string app_name,
+          string description,
+          checksum256 username_hash,
+          string logo_url,
+          string origin,
+          public_key key);
+
+      /**
+       * Adds a new key to a person's account to log into an app with
+       *
+       * @param account - account of the person
+       * @param app - account of the app to authorize the key to
+       * @param key - public key to authorize
+       */
+      [[eosio::action]] void loginwithapp(
+          name account,
+          name app,
+          name parent,
+          public_key key);
+
+      /**
+       * Update a key of a person
+       *
+       * @param account - name of the account to update
+       * @param permission - permission level of the key to update
+       * @param key - public key to update
+       */
+      [[eosio::action]] void updatekeyper(name account,
+                                          permission_level_name permission,
+                                          public_key key,
+                                          bool link_auth = false);
+
+      struct [[eosio::table]] account_type_struct
+      {
+         name account_name;
+         account_type acc_type;
+         uint16_t version; // used for upgrading the account structure
+
+         uint64_t primary_key() const { return account_name.value; }
+         EOSLIB_SERIALIZE(struct account_type_struct, (account_name)(acc_type)(version))
+      };
+
+      typedef eosio::multi_index<"acctypes"_n, account_type_struct> account_type_table;
+
+      TABLE person
+      {
+         name account_name;
+         account_status status;
+         checksum256 username_hash;
+         checksum256 password_salt;
+
+         // primary key automatically added by EOSIO method
+         uint64_t primary_key() const { return account_name.value; }
+         // also index by username hash
+         checksum256 index_by_username_hash() const { return username_hash; }
+      };
+
+      // Create a multi-index-table with two indexes
+      typedef eosio::multi_index<"people"_n, person,
+                                 eosio::indexed_by<"usernamehash"_n, eosio::const_mem_fun<person, checksum256, &person::index_by_username_hash>>>
+          people_table;
+
+      // Create an instance of the table that can is initalized in the constructor
+      people_table _people;
+
+      TABLE app
+      {
+         name account_name;
+         string app_name;
+         checksum256 username_hash;
+         string description;
+         string logo_url;
+         string origin;
+
+         // primary key automatically added by EOSIO method
+         uint64_t primary_key() const { return account_name.value; }
+         // also index by username hash
+         checksum256 index_by_username_hash() const { return username_hash; }
+         checksum256 index_by_origin_hash() const { return eosio::sha256(origin.c_str(), std::strlen(origin.c_str())); }
+      };
+
+      // Create a multi-index-table with two indexes
+      typedef eosio::multi_index<"apps"_n, app,
+                                 eosio::indexed_by<"usernamehash"_n,
+                                                   eosio::const_mem_fun<app, checksum256, &app::index_by_username_hash>>,
+                                 eosio::indexed_by<"originhash"_n,
+                                                   eosio::const_mem_fun<app, checksum256, &app::index_by_origin_hash>>>
+          apps_table;
+
+      // Create an instance of the table that can is initalized in the constructor
+      apps_table _apps;
+
+      /**
+       * Returns the account name of the app that corresponds to the origin
+       *
+       * @param {string} origin - the origin of the app
+       * @example "https://www.tonomy.com"
+       * @param {name} [contract_name] - the name of the contract to query
+       * @returns {name} - the account name of the app
+       */
+      static const name get_app_permission_by_origin(string origin, name contract_name = "id.tmy"_n)
+      {
+         apps_table id_apps = apps_table(contract_name, contract_name.value);
+         auto apps_by_origin_hash_itr = id_apps.get_index<"originhash"_n>();
+
+         eosio::checksum256 origin_hash = eosio::sha256(origin.c_str(), std::strlen(origin.c_str()));
+         const auto origin_itr = apps_by_origin_hash_itr.find(origin_hash);
+         check(origin_itr == apps_by_origin_hash_itr.end(), "No app with this origin found");
+
+         return origin_itr->account_name;
+      }
+
+      /**
+       * Returns the account name of the app that corresponds to the origin
+       *
+       * @param {string} username - the username of the app
+       * @example "demo.app.tonomy.id"
+       * @param {name} [contract_name] - the name of the contract to query
+       * @returns {name} - the account name of the app
+       */
+      static const name get_app_permission_by_username(string username, name contract_name = "id.tmy"_n)
+      {
+         apps_table id_apps = apps_table(contract_name, contract_name.value);
+         auto apps_by_username_hash_itr = id_apps.get_index<"usernamehash"_n>();
+
+         eosio::checksum256 username_hash = eosio::sha256(username.c_str(), std::strlen(username.c_str()));
+         const auto username_itr = apps_by_username_hash_itr.find(username_hash);
+         check(username_itr == apps_by_username_hash_itr.end(), "No app with this username found");
+
+         return username_itr->account_name;
+      }
 
       struct [[eosio::table]] abi_hash
       {
@@ -309,20 +525,18 @@ namespace eosiobiostonomy
 
       typedef eosio::multi_index<"abihash"_n, abi_hash> abi_hash_table;
 
-      struct [[eosio::table]] resource_config {
-          double ram_fee; // RAM fee fraction (0.01 = 1% fee)
-          double ram_price; // RAM price in tokens/byte
-          uint64_t total_ram_available; // Total available RAM in bytes
-          uint64_t total_ram_used; // Total RAM used in bytes
-          uint64_t total_cpu_weight_allocated; // Total allocated CPU weight
-          uint64_t total_net_weight_allocated; // Total allocated NET weight
-         
-         EOSLIB_SERIALIZE(resource_config, (ram_price)(total_ram_available)(total_ram_used)
-         (total_cpu_weight_allocated)(total_net_weight_allocated)(ram_fee)
-         )
+      struct [[eosio::table]] resource_config
+      {
+         double ram_fee;                      // RAM fee fraction (0.01 = 1% fee)
+         double ram_price;                    // RAM price in tokens/byte
+         uint64_t total_ram_available;        // Total available RAM in bytes
+         uint64_t total_ram_used;             // Total RAM used in bytes
+         uint64_t total_cpu_weight_allocated; // Total allocated CPU weight
+         uint64_t total_net_weight_allocated; // Total allocated NET weight
+
+         EOSLIB_SERIALIZE(resource_config, (ram_price)(total_ram_available)(total_ram_used)(total_cpu_weight_allocated)(total_net_weight_allocated)(ram_fee))
       };
       typedef eosio::singleton<"resconfig"_n, resource_config> resource_config_table;
-
 
       using newaccount_action = action_wrapper<"newaccount"_n, &bios::newaccount>;
       using updateauth_action = action_wrapper<"updateauth"_n, &bios::updateauth>;
@@ -341,5 +555,19 @@ namespace eosiobiostonomy
       using reqactivated_action = action_wrapper<"reqactivated"_n, &bios::reqactivated>;
       using setresourceparams_action = action_wrapper<"setresparams"_n, &bios::setresparams>;
       using buyram_action = action_wrapper<"buyram"_n, &bios::buyram>;
+
+      using newperson_action = action_wrapper<"newperson"_n, &bios::newperson>;
+      using updatekeyper_action = action_wrapper<"updatekeyper"_n, &bios::updatekeyper>;
    };
 }
+
+void throwError(std::string error_code, std::string message)
+{
+   eosio::check(false, error_code + ": " + message);
+}
+
+/*
+List of which files have which error codes, to avoid duplicates:export function throwError(string error_code, string message) {
+
+TCON10## = id.tmy.cpp
+*/
