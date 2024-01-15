82ddb713b0b85360398770847252f002f8ebdb6e
Jack Tanner <jack@jackandtheblockstalk.com>
2024-01-15 17:30:09 +0100

    feat: Fixed native contract auth

--- contracts/tonomy/build.sh
+++ contracts/tonomy/build.sh
@@ -5,16 +5,20 @@ BUILD_METHOD=$1
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "${PARENT_PATH}"
 
-# source ../compile_contract.sh
-
-# compile_contract "${PARENT_PATH}" "tonomy" "${BUILD_METHOD}"
-
 CONTRACT_NAME="tonomy"
 
-WORKING_DIR="/contracts"
+if [ "$BUILD_METHOD" == "local" ]; then
+    WORKING_DIR="."
+else
+    WORKING_DIR="/contracts"
+fi
 
 BUILD_COMMAND="cdt-cpp -abigen -I ${WORKING_DIR}/include -R ${WORKING_DIR}/ricardian -contract ${CONTRACT_NAME} -o ${WORKING_DIR}/${CONTRACT_NAME}.wasm ${WORKING_DIR}/src/${CONTRACT_NAME}.cpp ${WORKING_DIR}/src/native.cpp"
 
 echo $BUILD_COMMAND
 
-docker run -v "${PARENT_PATH}:${WORKING_DIR}" antelope_blockchain bash -c "${BUILD_COMMAND}"
+if [ "$BUILD_METHOD" == "local" ]; then
+    bash -c "${BUILD_COMMAND}"
+else
+    docker run -v "${PARENT_PATH}:${WORKING_DIR}" antelope_blockchain bash -c "${BUILD_COMMAND}"
+fi
--- contracts/tonomy/src/native.cpp
+++ contracts/tonomy/src/native.cpp
@@ -14,8 +14,8 @@ namespace tonomysystem
 
    void native::newaccount(name creator, name name, authority owner, authority active)
    {
-      require_auth(governance_name);
-      native::newaccount_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::newaccount_action action("eosio"_n, {creator, "active"_n});
       action.send(creator, name, owner, active);
    }
 
@@ -24,16 +24,16 @@ namespace tonomysystem
                            name parent,
                            authority auth)
    {
-      require_auth(governance_name);
-      native::updateauth_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::updateauth_action action("eosio"_n, {account, permission});
       action.send(account, permission, parent, auth);
    }
 
    void native::deleteauth(name account,
                            name permission)
    {
-      require_auth(governance_name);
-      native::deleteauth_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::deleteauth_action action("eosio"_n, {account, permission});
       action.send(account, permission);
    }
 
@@ -42,8 +42,8 @@ namespace tonomysystem
                          name type,
                          name requirement)
    {
-      require_auth(governance_name);
-      native::linkauth_action action("eosio"_n, {get_self(), "active"_n});
+      // TODO: this should be governance authorized only. Create a separate action in tonomy.cpp for People accounts to link their SSO account
+      native::linkauth_action action("eosio"_n, {account, "active"_n});
       action.send(account, code, type, requirement);
    }
 
@@ -51,90 +51,90 @@ namespace tonomysystem
                            name code,
                            name type)
    {
-      require_auth(governance_name);
-      native::unlinkauth_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::unlinkauth_action action("eosio"_n, {account, "active"_n});
       action.send(account, code, type);
    }
 
    void native::canceldelay(permission_level canceling_auth, checksum256 trx_id)
    {
-      require_auth(governance_name);
-      native::canceldelay_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::canceldelay_action action("eosio"_n, {governance_name, "active"_n});
       action.send(canceling_auth, trx_id);
    }
 
    void native::setcode(name account, uint8_t vmtype, uint8_t vmversion, const std::vector<char> &code)
    {
-      require_auth(governance_name);
-      native::setcode_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::setcode_action action("eosio"_n, {account, "active"_n});
       action.send(account, vmtype, vmversion, code);
    }
 
    void native::setabi(name account, const std::vector<char> &abi)
    {
-      require_auth(governance_name);
-      native::setabi_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::setabi_action action("eosio"_n, {account, "active"_n});
       action.send(account, abi);
    }
 
    void native::setpriv(name account, uint8_t is_priv)
    {
       // TODO disable proxying: this contract is priviledged and can execute the required API calls directly
-      require_auth(governance_name);
-      native::setpriv_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::setpriv_action action("eosio"_n, {governance_name, "active"_n});
       action.send(account, is_priv);
    }
 
    void native::setalimits(name account, int64_t ram_bytes, int64_t net_weight, int64_t cpu_weight)
    {
       // TODO delete function. This is handled by our own resource management
-      require_auth(governance_name);
-      native::setalimits_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::setalimits_action action("eosio"_n, {governance_name, "active"_n});
       action.send(account, ram_bytes, net_weight, cpu_weight);
    }
 
    void native::setprods(const std::vector<eosio::producer_authority> &schedule)
    {
-      require_auth(governance_name);
-      native::setprods_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::setprods_action action("eosio"_n, {governance_name, "active"_n});
       action.send(schedule);
    }
 
    void native::setparams(const eosio::blockchain_parameters &params)
    {
       // TODO disable proxying: this contract is priviledged and can execute the required API calls directly
-      require_auth(governance_name);
-      native::setparams_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::setparams_action action("eosio"_n, {governance_name, "active"_n});
       action.send(params);
    }
 
    void native::reqauth(name from)
    {
       // TODO delete as not needed. Check in Telegram first
-      require_auth(governance_name);
-      native::reqauth_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::reqauth_action action("eosio"_n, {from, "active"_n});
       action.send(from);
    }
 
    void native::activate(const eosio::checksum256 &feature_digest)
    {
-      require_auth(governance_name);
-      native::activate_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::activate_action action("eosio"_n, {governance_name, "active"_n});
       action.send(feature_digest);
    }
 
    void native::reqactivated(const eosio::checksum256 &feature_digest)
    {
-      require_auth(governance_name);
-      native::reqactivated_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::reqactivated_action action("eosio"_n, {governance_name, "active"_n});
       action.send(feature_digest);
    }
 
    void native::onerror(uint128_t sender_id, std::vector<char> sent_trx)
    {
       // TODO delete: this is not needed in this contract. It is not supposed to be called in the eosio contract
-      require_auth(governance_name);
-      native::onerror_action action("eosio"_n, {get_self(), "active"_n});
+      require_auth(governance_name); // gov only, till we implement this
+      native::onerror_action action("eosio"_n, {governance_name, "active"_n});
       action.send(sender_id, sent_trx);
    }
 
