4f8ec60b5e7df10d7e9967a344dd949494abaea8
sadiabbasi <sadiaabbas326@gmail.com>
2024-01-17 15:00:27 +0300

    fixed comments

--- blockchain/transact.sh
+++ blockchain/transact.sh
@@ -6,10 +6,8 @@ read -s -p "Enter Private Key: " private_key
 # Import the key into the EOSIO wallet
 cleos wallet import --private-key $private_key
 
-echo $private_key
-
 # Get the wallet alias
-read -s -p "Enter alias url: " alias_url
+read -s -p ""Enter blockchain API url (e.g. https://blockchain-api.pangea.web4.world): " alias_url
 
 # create an alias so that it connects to pangea mainnet
 alias cleos='cleos -u' $alias_url
