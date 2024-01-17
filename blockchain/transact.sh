07c7414b7db8f9be2a31b85f0b708110c164af5e
sadiabbasi <sadiaabbas326@gmail.com>
2024-01-17 15:56:07 +0300

    update document

--- blockchain/transact.sh
+++ blockchain/transact.sh
@@ -7,7 +7,7 @@ read -s -p "Enter Private Key: " private_key
 cleos wallet import --private-key $private_key
 
 # Get the wallet alias
-read -s -p ""Enter blockchain API url (e.g. https://blockchain-api.pangea.web4.world): " alias_url
+read -p "Enter blockchain API url (e.g. https://blockchain-api.pangea.web4.world): " alias_url
 
 # create an alias so that it connects to pangea mainnet
 alias cleos='cleos -u' $alias_url
