01264acf726cc01f7f3f1bdcf4bb0e2704294bcb
sadiabbasi <sadiaabbas326@gmail.com>
2025-05-05 11:42:12 +0300

    fix: :fire: update equal symbol

--- contracts/vesting.tmy/src/vesting.tmy.cpp
+++ contracts/vesting.tmy/src/vesting.tmy.cpp
@@ -218,7 +218,7 @@ namespace vestingtoken
 
         for (auto iter = vesting_table.begin(); iter != vesting_table.end(); ++iter)
         {
-            if (iter->tokens_allocated.symbol === SYSTEM_RESOURCE_CURRENCY_OLD)
+            if (iter->tokens_allocated.symbol == SYSTEM_RESOURCE_CURRENCY_OLD)
             {
                 vesting_table.modify(iter, get_self(), [&](auto &row)
                                  {
